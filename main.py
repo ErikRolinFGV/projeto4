6bda99520eba742bb566cce61758c473c593bfd4
ErikRolinFGV <erik7nonato@gmail.com>
2025-09-17 12:13:11 -0300

    Estrutura pronta

---
+++ main.py
@@ -0,0 +1,11 @@
+# API Framework
+from fastapi import FastAPI
+
+# Manipulação de dados (se necessário)
+import pandas as pd
+
+# Para execução da API
+import uvicorn
+
+# Utilitários (se necessário)
+import json
